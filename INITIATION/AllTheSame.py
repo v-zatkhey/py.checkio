bb19b541fd9f82816b7330ed356e1ddc78fec089
Volodymyr Zatkhey <zatkhey@gmail.com>
2025-05-02 18:19:47 +0300

    2/05/2025 18:19

--- INITIATION/AllTheSame.py
+++ INITIATION/AllTheSame.py
@@ -11,6 +11,12 @@ def all_the_same(elements: List[Any]) -> bool:
                     return False
     return True
 
+# def all_the_same(elements: List[Any]) -> bool:
+#     return len(set(elements)) <= 1
+
+# def all_the_same(elements: List[Any]) -> bool:
+#     return all(elements[i]==elements[i+1] for i in range(len(elements)-1))
+
 
 print("Example:")
 print(all_the_same([1, 1, 1]))
