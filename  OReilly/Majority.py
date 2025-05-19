0e9ae4cdae204ca7bb7a9b8ea5392591efecf607
Volodymyr Zatkhey <zatkhey@gmail.com>
2025-05-19 08:34:48 +0300

    19/05/2025 08:34

---
+++  OReilly/Majority.py
@@ -0,0 +1,19 @@
+def is_majority(items: list[bool]) -> bool:
+    if len(items) == 0:
+        return False
+    return sum(map(int, items)) > len(items) // 2
+
+
+print("Example:")
+print(is_majority([True, True, False, True, False]))
+
+# These "asserts" are used for self-checking
+assert is_majority([True, True, False, True, False]) == True
+assert is_majority([True, True, False]) == True
+assert is_majority([True, True, False, False]) == False
+assert is_majority([True, True, False, False, False]) == False
+assert is_majority([False]) == False
+assert is_majority([True]) == True
+assert is_majority([]) == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
